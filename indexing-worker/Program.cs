d6ad3664aff7a5ecc699491e20b4ce477157b7a9
tientt010 <bechanhtt010@gmail.com>
2026-07-20 14:04:47 +0700

    feat: add data seeder with --seed and --clear commands

--- indexing-worker/Program.cs
+++ indexing-worker/Program.cs
@@ -5,7 +5,19 @@ var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddHttpClient();
 builder.Services.AddSingleton<ImageProcessor>();
+builder.Services.AddSingleton<DataSeeder>();
+
+// Nếu có argument --seed thì chạy DataSeeder rồi thoát
+if (args.Contains("--seed"))
+{
+    var shouldClear = args.Contains("--clear");
+    var host = builder.Build();
+    var seeder = host.Services.GetRequiredService<DataSeeder>();
+    await seeder.RunAsync(shouldClear, CancellationToken.None);
+    return;
+}
+
 builder.Services.AddHostedService<Worker>();
 
-var host = builder.Build();
-host.Run();
+var app = builder.Build();
+app.Run();
